9d6cccab392efc1d81e98935764e90380befb7d8
Bhawani Singh <bs190520@gmail.com>
2025-02-11 20:25:23 +0530

    Added Home and Signup Page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+
+
+function Home() {
+  return (
+    <>
+    <div className="flex text-red-700 font-bold justify-center text-4xl ">
+      <h1 className="text-black">Welcome to RIM</h1>X
+    </div>
+    </>
+  )
+}
+
+export default Home
